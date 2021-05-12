3a2216cfc3680035baf2c5051e8fa80cdd900098
John Eagan <johnjeagan@mail.fresnostate.edu>
2021-05-11 20:40:18 -0700

    dd

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import multiprocessing
+from multiprocessing import Process
 
+def f(num):
+    while True:
+        print(num)
+
+if __name__ == '__main__':
+    p = Process(target=f, args=(1,))
+    p2 = Process(target=f, args=(2,))
+    p.start()
+    p2.start()
+    p.join()
+    p2.join()
